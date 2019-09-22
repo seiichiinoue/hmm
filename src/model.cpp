6995bd404a91363f27dd4a1227451fa834113b7a
Seiichi Inoue <d35inou108@gmail.com>
2019-09-22 23:38:02 +0900

    added model

---
+++ src/model.cpp
@@ -0,0 +1,21 @@
+#include <set>
+#include <algorithm>
+#include <boost/foreach.hpp>
+#include "hmm.hpp"
+
+DEFINE_int32(N, 10, "Number of hidden stete");
+DEFINE_string(DATA, "data/wiki-example.txt", "Data file");
+
+int main(int argc, char *argv[]) {
+    google::InitGoogleLogging(*argv);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    HMM hmm;
+    HMM::Observation o;
+    
+    hmm.read(FLAGS_DATA, o,  true);
+    hmm.train(FLAGS_N);
+    hmm.forward_backward(o);
+    hmm.viterbi(o);
+    return 0;
+}
