88a199075ae229a0bb4a773d08ad3901746544e9
Seiichi Inoue <d35inou108@gmail.com>
2019-09-25 01:52:00 +0900

    added text processing metho

--- utils/text.py
+++ utils/text.py
@@ -3,8 +3,8 @@ import MeCab
 class Text(object):
     def __init__(self, path="data/wiki.txt"):
         self.wakati = MeCab.Tagger('-Owakati')
-        self.chasen = MeCab.Tagger()
-        with open(path, "r") as f:
+        self.chasen = MeCab.Tagger('-Ochasen')
+        with open(path, "r", encoding="utf-8") as f:
             self.text = f.readlines()
         for i in range(len(self.text)):
             self.text[i] = self.text[i].replace("\n", "")
@@ -18,13 +18,17 @@ class Text(object):
         with open(path, "w") as f:
             for t in wakatied:
                 f.write(t)
+        return None
 
-    def _morpho_analysis(self):
-        return
-
-t = Text("./data/neko.txt")
-# print(t.text[0].strip("\n"))
-# print(t.text)
-ret = t._wakati(path="./data/neko-wakati.txt")
-# print(ret)
-t._morpho_analysis()
+    def _morpho_analysis(self, path):
+        self.chasen.parse("")
+        with open(path, "w") as f:
+            for i in range(len(self.text)):
+                node = self.chasen.parseToNode(self.text[i])
+                while node:
+                    sf = node.surface
+                    pos = node.feature.split(",")[0]
+                    if sf != '':
+                        f.write(sf+'/'+pos+' ')
+                    node = node.next
+        return None
