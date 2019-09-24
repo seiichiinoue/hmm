816f1dc969244c2143e38b5697cd1715234209d8
Seiichi Inoue <d35inou108@gmail.com>
2019-09-25 02:07:22 +0900

    modified

--- src/test.cpp
+++ src/test.cpp
@@ -4,7 +4,7 @@
 #include "hmm.hpp"
 
 DEFINE_int32(N, 10, "Number of hidden stete");
-DEFINE_string(DATA, "../data/wiki-sample.word", "Data file");
+DEFINE_string(DATA, "/data/wiki-sample.word", "Data file");
 
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(*argv);
